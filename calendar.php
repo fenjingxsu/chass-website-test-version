acc236963c6b3e2fd6663e28e9f5cc324b8c15f1
fenjingxsu <fjx850402@gmail.com>
2019-03-04 13:05:17 +0800

    first commit

---
+++ calendar.php
@@ -0,0 +1,132 @@
+<html>
+<head>
+</head>
+<body onload="generate_table()">
+	<div id="cal_table_container">
+		<div id="month_container">
+			<span id="pre_month_arrow" onclick="pre_month()"><</span>
+			<span id="month_text"></span>
+			<span id="next_month_arrow" onclick="next_month()">></span>
+		</div>
+		<table id="cal_table" border = "1">
+			<tr><td>SUN</td><td>MON</td><td>TUE</td><td>WED</td><td>THU</td><td>FRI</td><td>SAT</td></tr>
+			<?
+				echo "<tr>";
+				for($i = 0; $i < 42; $i++)
+				{
+					echo "<td id='cal_".$i."'></td>";
+					if($i % 7 == 6) echo "</tr><tr>";
+				}
+				echo "<tr>";
+			?>
+		</table>
+	</div>
+
+	<script>
+		var month_head;
+		var month_tmp; //js month是0-11所以帶入要-1
+		var year_tmp;
+		function new_cal()
+		{
+			var date_MAX;
+			if(month_tmp == 1 || month_tmp == 3 || month_tmp == 5 || month_tmp == 7 || month_tmp == 8 ||  month_tmp == 10 ||  month_tmp == 12)
+				date_MAX = 31;
+			else if(month_tmp == 4 ||  month_tmp == 6 || month_tmp == 9 ||  month_tmp == 11)
+				date_MAX = 30;
+			else if(month_tmp == 2)
+			{
+				if(year_tmp % 4 == 0)
+					date_MAX = 29;
+				else
+					date_MAX = 28;
+			}
+			var date_tmp = 0;
+			for(i = 0; i < 42; i++)
+			{
+				document.getElementById("cal_"+i).innerHTML = ""; //清空所有格子
+				if(i  >= month_head && date_tmp < date_MAX)
+				{
+					document.getElementById("cal_"+(month_head+date_tmp)).innerHTML = date_tmp+1;
+					date_tmp++;
+				}
+			}
+
+			//檢查一整行空著
+			var check_empty_top = 0;
+			var check_empty_btm = 0;
+			for(i = 0; i < 7; i++)
+			{
+				if(document.getElementById("cal_"+i).innerHTML != "")
+					check_empty_top = 1;
+				var p = i+36;
+				if(document.getElementById("cal_"+p).innerHTML != "")
+					check_empty_btm = 1;
+			}
+			if(check_empty_top == 0)
+			{
+				for(i = 0; i < 7; i++)
+					document.getElementById("cal_"+i).style.height = 0;
+			}
+			if(check_empty_btm == 0)
+			{
+				for(i = 36; i < 43; i++)
+					document.getElementById("cal_"+i).style.height = 0;
+			}
+
+		}
+		function generate_table()
+		{
+			var i;
+			var d = new Date();
+			d.setDate(1);
+			month_head = d.getDay();
+			month_tmp = d.getMonth()+1;
+			document.getElementById("month_text").innerHTML = month_tmp;
+			year_tmp = d.getFullYear();
+
+			new_cal();
+		}
+		function pre_month()
+		{
+			var i;
+			var d = new Date();
+			d.setDate(1);
+			
+			if(month_tmp == 1)
+			{
+				year_tmp--;
+				d.setFullYear(year_tmp);
+				month_tmp = 12;
+			}
+			else
+				month_tmp--;
+			document.getElementById("month_text").innerHTML = month_tmp;
+			d.setMonth(month_tmp-1);
+			month_head = d.getDay();
+
+			new_cal();
+		}
+		function next_month()
+		{
+			var i;
+			var d = new Date();
+			d.setDate(1);
+			
+			if(month_tmp == 12)
+			{
+				year_tmp++;
+				d.setFullYear(year_tmp);
+				month_tmp = 1;
+			}
+			else
+				month_tmp++;
+			document.getElementById("month_text").innerHTML = month_tmp;
+			d.setMonth(month_tmp-1);
+			month_head = d.getDay();
+
+			new_cal();
+		}
+	</script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,99 @@
+<html>
+<head>
+	<style type="text/css">
+	@import url(https://fonts.googleapis.com/earlyaccess/cwtexfangsong.css);
+	body{
+		background-image: url(img/backgroundlong.png);
+		background-repeat: repeat-x;
+		margin: 0;
+		font-family: 'cwTeXFangSong', serif;
+	}
+	#main_content{
+		width: 44%;
+		background-color: white;
+		position: absolute;
+		left: 28%;
+	}
+	#top_bar{
+		text-align: right;
+		//box-shadow: 0px 1px 1px black;
+	}
+	#top_bar a{
+		text-decoration: none;
+	}
+	#title{
+		background-image: url(img/title.png);
+		background-repeat: no-repeat;
+		background-position: center;
+		height: 10rem;
+	}
+	#btn_bar{
+		height: 3rem;
+		background-image: linear-gradient(#e2e2e2, white, #e2e2e2); 
+		text-align: center;
+	}
+	#btn_bar a{
+		font-family: 'cwTeXFangSong', serif;
+		height: 3rem;
+		line-height: 3rem;
+		font-size: 1.2rem;
+		font-weight: 600;
+		padding-right: 1rem;
+		color: black;
+		text-decoration: none;
+	}
+	#btn_bar a:hover{
+		filter: drop-shadow(0px 5px 5px #a59166)
+	}
+	#pic_bar{
+		height: 15rem;
+		background-repeat: no-repeat;
+		background-position: center;
+	}
+	#small_title{
+		height: 5rem;
+		font-size: 2rem;
+		font-weight: 600;
+		line-height: 5rem;
+	}
+	</style>
+</head>
+<body onload="load_pic_bar()">
+	<div id='main_content'>
+		<div id="top_bar">
+			<a href="https://web.ncku.edu.tw/index.php">成大首頁</a>
+			︱
+			<a>English</a>&nbsp
+		</div>
+		<div id="title">
+		</div>
+		<div id="btn_bar">
+			<a href="index.php">最新消息</a>
+			<a>中心簡介</a>
+			<a>學術活動</a>
+			<a>研究計畫</a>
+			<a>執行成果</a>
+			<a>相關連結</a>
+			<a>網站地圖</a>
+		</div>
+		<div id="pic_bar">
+		</div>
+		<div id="small_title">
+			&nbsp最新消息
+		</div>
+		<div id="content">
+		</div>
+		<div id="bottom_bar">
+		</div>
+	</div>
+
+	<script type="text/javascript">
+		function load_pic_bar()
+		{
+			var pic_num = Math.floor((Math.random() * 10));
+			document.getElementById("pic_bar").style.backgroundImage = "url('pic_bar/"+pic_num+".png')";
+		}
+	</script>
+
+</body>
+</html>
